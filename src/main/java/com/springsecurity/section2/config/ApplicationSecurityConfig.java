dec1dd944f641859864213da4dfa7d3d743b5d38
suresh <sureshmjava83@gmail.com>
2023-07-08 00:42:49 +0530

    Update the section3 related to security

--- src/main/java/com/springsecurity/section2/config/ApplicationSecurityConfig.java
+++ src/main/java/com/springsecurity/section2/config/ApplicationSecurityConfig.java
@@ -3,6 +3,11 @@ package com.springsecurity.section2.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -33,7 +38,27 @@ public class ApplicationSecurityConfig {
                 .anyRequest().permitAll()
                 .and().formLogin()
                 .and().httpBasic();*/
-        
+
+
+
         return httpSecurity.build();
+    }
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(){
+        UserDetails admin= User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("12345")
+                .authorities("admin")
+                .build();
+        UserDetails user=User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .authorities("read")
+                .build();
+        return new InMemoryUserDetailsManager(admin,user);
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
